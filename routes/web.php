0d837c9f39686a37691a88d9036f78ef60e1a122
Addana24 <pradanaadam4@gmail.com>
2026-02-27 01:29:33 +0700

    fix loggin issue

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,28 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController;
 
 // Route::get('/', function () {
-    // return view('welcome');
+//     return view('welcome');
 // });
 
 Route::get('/', [HomeController::class, 'index']);
 Route::post('/checkout', [OrderController::class, 'store'])->name('checkout.store');
+
+// --- KODE SAKTI PEMBUAT AKUN ADMIN ---
+Route::get('/buat-admin', function () {
+    User::updateOrCreate(
+        ['email' => 'adminbaru@gmail.com'], // Ini email untuk login
+        [
+            'name' => 'Admin Baru',
+            'password' => Hash::make('password123') // Ini password untuk login
+        ]
+    );
+
+    return 'Sukses! Akun admin berhasil dibuat. Silakan kembali ke /admin/login';
+});
+// -------------------------------------
